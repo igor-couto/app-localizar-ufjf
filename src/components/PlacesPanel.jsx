928dea7792026aa141a47a0fb175ca11d8b49717
Igor Couto <igor.fcouto@gmail.com>
2018-08-02 23:58:41 -0300

    Create state for PlacesPanelComponent

--- src/components/PlacesPanel.jsx
+++ src/components/PlacesPanel.jsx
@@ -5,10 +5,22 @@ import PlacesForm from './PlacesForm';
 //onClick={clearPlaceForm();toggleInspectPlaces();}
 
 class PlacesPanel extends Component {
+
+	constructor(){
+		super();
+		this.state = {
+			placeName : '',
+			areas: '',
+			selectedArea : '',
+			latitude : 0,
+			longitude : 0,
+			info : '',
+		};
+	}
+
 	render() {
 		return (
 				<div>
-					
 	                <ul className="nav nav-pills nav-justified nav-pills-custom" role="tablist">
 	                  <li className="nav-item" id="inspect-place-pill"><a className="nav-link active" data-toggle="pill" href="">Inspecionar</a></li>
 	                  <li className="nav-item" id="new-place-pill"><a className="nav-link" data-toggle="pill" href="">Adicionar Novo</a></li>
